65fabe1d06537b0ed7a9279d43eccd4f41da7484
Bo Henderson <bohende@gmail.com>
2018-09-08 10:10:01 +0200

    Fix dharam contact migrations

---
+++ migrations/3_deploy_terms_contracts.js
@@ -0,0 +1,10 @@
+module.exports = (deployer, network, accounts) => {
+    const ContractRegistry = artifacts.require("ContractRegistry");
+    const SimpleInterestTermsContract = artifacts.require("SimpleInterestTermsContract");
+    const CollateralizedSimpleInterestTermsContract = artifacts.require(
+        "CollateralizedSimpleInterestTermsContract",
+    );
+
+    deployer.deploy(SimpleInterestTermsContract, ContractRegistry.address);
+    deployer.deploy(CollateralizedSimpleInterestTermsContract, ContractRegistry.address);
+};
