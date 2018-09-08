f4f3fdedaedaf97ee658d698b1a4a64d6041d20e
Bo Henderson <bohende@gmail.com>
2018-09-08 17:56:02 +0200

    Abandon alchemy

--- src/App.spec.tsx
+++
@@ -1,8 +0,0 @@
-import {render} from 'react-dom';
-import * as React from 'react';
-import { App } from './App';
-
-it('renders without crashing', async () => {
-  const div = document.createElement('div');
-  render(<App />, div);
-});
